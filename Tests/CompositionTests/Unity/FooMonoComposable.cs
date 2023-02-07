b99a0c730d838bbacf33946305280aaf95547944
Semih Celek <cybermoon1999@gmail.com>
2023-02-07 21:43:38 +0300

    add monocomposable

---
+++ Tests/CompositionTests/Unity/FooMonoComposable.cs
@@ -0,0 +1,13 @@
+﻿using Gum.Composer;
+using Gum.Composer.Unity.Runtime;
+
+namespace Tests.CompositionTests.Unity
+{
+    public class FooMonoComposable : MonoComposable 
+    {
+        protected override IAspect[] GetAspects()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
