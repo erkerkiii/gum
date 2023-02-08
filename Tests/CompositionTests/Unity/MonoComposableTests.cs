1c1f4b4fd769e5bc87186c298c39a9d0f8297c75
Semih Celek <cybermoon1999@gmail.com>
2023-02-08 22:25:04 +0300

    write test for mono composable

--- Tests/CompositionTests/Unity/MonoComposableTests.cs
+++ Tests/CompositionTests/Unity/MonoComposableTests.cs
@@ -1,9 +1,9 @@
 #if UNITY_2019_1_OR_NEWER
-using System.Collections;
+using Gum.Composer;
+using Gum.Composer.Exception;
 using Gum.Composer.Unity.Runtime;
 using NUnit.Framework;
 using UnityEngine;
-using UnityEngine.TestTools;
 
 namespace Tests.CompositionTests.Unity
 {
@@ -11,9 +11,9 @@ namespace Tests.CompositionTests.Unity
     {
         private MonoComposable _mockMonoComposable;
 
-        private const int Value = 12;
+        private const int VALUE = 12;
 
-        [OneTimeSetUp]
+        [SetUp]
         public void SetUp()
         {
             GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
@@ -21,23 +21,109 @@ namespace Tests.CompositionTests.Unity
             _mockMonoComposable = fooMonoComposable;
         }
 
-        [UnityTest]
-        public IEnumerator Get_Aspect_From_MonoComposable()
+        [Test]
+        public void Add_Aspect_To_MonoComposable()
         {
-            FooAspect fooAspect = _mockMonoComposable.Composition.GetAspect<FooAspect>();
-            Assert.AreEqual(Value, fooAspect.MyInt);
+            Assert.IsFalse(_mockMonoComposable.Composition.HasAspect(BarAspect.ASPECT_TYPE));
             
-            yield return null;
+            _mockMonoComposable.Composition.AddAspect(new BarAspect(VALUE));
+            
+            Assert.IsTrue(_mockMonoComposable.Composition.HasAspect(BarAspect.ASPECT_TYPE));
         }
 
-        [UnityTest]
-        public IEnumerator Try_Get_Aspect_From_MonoComposable()
+        [Test]
+        public void MonoComposable_Has_Aspect()
+        {
+            Assert.IsTrue(_mockMonoComposable.Composition.HasAspect(FooAspect.ASPECT_TYPE));
+        }
+        
+        [Test]
+        public void Get_Aspect_From_MonoComposable()
+        {
+            FooAspect fooAspect = _mockMonoComposable.Composition.GetAspect<FooAspect>();
+            Assert.AreEqual(VALUE, fooAspect.MyInt);
+        }
+
+        [Test]
+        public void Try_Get_Aspect_From_MonoComposable()
         {
             Assert.IsTrue(_mockMonoComposable.Composition.TryGetAspect(out FooAspect fooAspect));
-            Assert.AreEqual(Value, fooAspect.MyInt);
+            Assert.AreEqual(VALUE, fooAspect.MyInt);
             Assert.IsFalse(_mockMonoComposable.Composition.TryGetAspect(out BarAspect _));
+        }
+
+        [Test]
+        public void Get_Aspect_Fluent()
+        {
+            _mockMonoComposable.Composition.GetAspectFluent(out FooAspect fooAspect);
             
-            yield return null;
+            Assert.AreEqual(VALUE, fooAspect.MyInt);
+        }
+
+        [Test]
+        public void Get_Aspect_With_Indexer()
+        {
+            _mockMonoComposable.Composition.AddAspect(new BarAspect(VALUE));
+            BarAspect barAspect = (BarAspect)_mockMonoComposable.Composition[BarAspect.ASPECT_TYPE];
+            
+            Assert.IsNotNull(barAspect);
+            Assert.AreEqual(VALUE, barAspect.MyInt);
+        }
+
+        [Test]
+        public void Set_Aspect()
+        {
+            FooAspect fooAspect = _mockMonoComposable.Composition.GetAspect<FooAspect>();
+            
+            Assert.AreEqual(VALUE, fooAspect.MyInt);
+
+            const int newValue = 20;
+            _mockMonoComposable.Composition.SetAspect(new FooAspect(newValue));
+            Assert.AreEqual(newValue, _mockMonoComposable.Composition.GetAspect<FooAspect>().MyInt);
+            Assert.AreEqual(1, _mockMonoComposable.Composition.AspectCount);
+        }
+        
+        [Test]
+        public void Set_Non_Existing_Aspect()
+        {
+            Assert.IsFalse(_mockMonoComposable.Composition.HasAspect(BarAspect.ASPECT_TYPE));
+
+            const int newValue = 20;
+            _mockMonoComposable.Composition.SetAspect(new BarAspect(newValue));
+            
+            Assert.AreEqual(newValue, _mockMonoComposable.Composition.GetAspect<BarAspect>().MyInt);
+            Assert.AreEqual(VALUE, _mockMonoComposable.Composition.GetAspect<FooAspect>().MyInt);
+            
+            Assert.AreEqual(2, _mockMonoComposable.Composition.AspectCount);
+        }
+
+        [Test]
+        public void Remove_Aspect()
+        {
+            Assert.IsTrue(_mockMonoComposable.Composition.HasAspect(FooAspect.ASPECT_TYPE));
+
+            _mockMonoComposable.Composition.RemoveAspect(FooAspect.ASPECT_TYPE);
+            
+            Assert.IsFalse(_mockMonoComposable.Composition.HasAspect(FooAspect.ASPECT_TYPE));
+        }
+
+        [Test]
+        public void Enumerator()
+        {
+            _mockMonoComposable.Composition.AddAspect(new BarAspect(VALUE));
+
+            foreach (IAspect aspect in _mockMonoComposable.Composition)
+            {
+                Assert.IsTrue(aspect is BarAspect || aspect is FooAspect);
+            }
+            
+            Assert.Pass();
+        }
+        
+        [TearDown]
+        public void TearDown()
+        {
+            Object.Destroy(_mockMonoComposable.gameObject);
         }
     }
 }
